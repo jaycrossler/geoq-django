90ece47db41bdf9c23714723cbda5be38562ff8d
Tyler Garner <garnertb@gmail.com>
2013-09-19 20:57:21 -0400

    Added edit map route from job detail and home url route.

--- geoq/urls.py
+++ geoq/urls.py
@@ -1,23 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.conf.urls import patterns, include, url
-
+from core.views import Dashboard
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'geoq.views.home', name='home'),
-    # url(r'^geoq/', include('geoq.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^$', Dashboard.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^geoq/', include('core.urls')),
     url(r'^maps/', include('maps.urls')),
-
     url(r'^accounts/$',
         'django.contrib.auth.views.login',),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login',),
