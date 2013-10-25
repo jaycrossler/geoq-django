46e004a62475dd5be9acc3e0d88d1cfaad525fef
jackiekazil <jackiekazil@gmail.com>
2013-10-25 14:24:13 -0400

    Initial guardian update.

--- geoq/core/admin.py
+++ geoq/core/admin.py
@@ -11,10 +11,10 @@
 # 'This software (GeoQ or Geographic Work Queueing and Tasking System)
 # is provided to the public as a courtesy of the National
 # Geospatial-Intelligence Agency.
-#  
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-#  
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -28,10 +28,16 @@ from django.contrib.gis import admin
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django import forms
+<<<<<<< HEAD
 from models import Project, Job, AOI, UserProfile
+=======
+from models import Project, Job, AOI
+from guardian.admin import GuardedModelAdmin
+>>>>>>> Initial guardian update.
 
 
-class ObjectAdmin(admin.OSMGeoAdmin, reversion.VersionAdmin):
+
+class ObjectAdmin(admin.OSMGeoAdmin, reversion.VersionAdmin,):
     list_display = ('name', 'created_at', 'updated_at')
 
 
@@ -64,7 +70,7 @@ class AOIAdmin(ObjectAdmin):
     rename_aois.short_description = "Rename AOIs"
 
 
-class JobAdmin(ObjectAdmin):
+class JobAdmin(GuardedModelAdmin, ObjectAdmin):
  	filter_horizontal = ("analysts","reviewers","feature_types")
  	save_on_top = True
 
