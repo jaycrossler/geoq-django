73d627dd6cccc0a7357fd2119ef1d47d1879d007
jackiekazil <jackiekazil@gmail.com>
2014-01-01 02:00:19 -0500

    Happy New Year! Adding email validation to users and orgs.

--- geoq/accounts/admin.py
+++ geoq/accounts/admin.py
@@ -1,28 +1,34 @@
 import reversion
 from django.contrib.gis import admin
-from models import UserAuthorization, UserProfile, Organization
+from models import EmailDomain, Organization, UserAuthorization, UserProfile
 
 
 class ObjectAdmin(reversion.VersionAdmin,):
     pass
 
+class EmailDomainInline(admin.TabularInline):
+    model = EmailDomain
+    extra = 5
+
+class EmailDomainAdmin(ObjectAdmin):
+    pass
 
 class OrganizationAdmin(ObjectAdmin):
+    inlines = [EmailDomainInline]
     pass
 
 # Unregister userena's admin to add to it.
 admin.site.unregister(UserProfile)
 class UserProfileAdmin(ObjectAdmin):
     list_display = ('user','organization','score')
-    # list_editable = ('organization','authorized',)
-    # readonly_fields = ('permissions_granted_by',)
+    readonly_fields = ('email',)
 
 class UserAuthorizationAdmin(ObjectAdmin):
     list_display = ('user_profile','authorized')
     list_editable = ('authorized',)
     readonly_fields = ('permissions_granted_by',)
 
-
+admin.site.register(EmailDomain, EmailDomainAdmin)
 admin.site.register(Organization, OrganizationAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(UserAuthorization, UserAuthorizationAdmin)
