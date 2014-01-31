6fc96c7ab211de1de4261d69e967a2a19e89ca31
jackiekazil <jackiekazil@gmail.com>
2014-01-31 13:42:11 -0500

    Checking and adding delete on the front-end fro one template.

--- geoq/core/middleware.py
+++ geoq/core/middleware.py
@@ -5,18 +5,13 @@ class UserPermsMiddleware(object):
 	def process_request(self, request):
 
 		"""
-		Populates user permissions.
+		Populates user permissions to use in the templates.
 		"""
 		user = request.user
 		perms = []
 
 		perms = list(user.get_all_permissions()) + perms
-
-		#print perms
-
 		request.base_perms = set(perms)
-
-		import logging
 		logging.info(request.base_perms)
 
 		return None
